308009c3c8ca659fb25c12b956b3a6ab158ff236
Ernest Phillips <ernestlphillips@gmail.com>
2019-03-12 15:09:15 -0700

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,28 +1,15 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+// https://medium.com/@dan_abramov/smart-and-dumb-components-7ca2f9a7c7d0
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
+import React from "react"
+import Form from './FormContainer'
+
+function App () {
+  return(
+    <div>
+      <Form />
+    </div>
+  )
 }
 
-export default App;
+
+export default App
